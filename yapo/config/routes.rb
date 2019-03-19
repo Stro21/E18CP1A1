2d85466e9d12614384885bed771337cfdd1e32e3
Eduardo Straub <eduardo.straub1@gmail.com>
2019-03-18 21:04:25 -0300

    La historia de usuario del campo usuario esta listo.

--- yapo/config/routes.rb
+++ yapo/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'users/search' #if method on UserController is search
   root to: 'users#search'
+  
 end
