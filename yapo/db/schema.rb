8e432baa6d979cae47d60ae7cd2c1c78b4651645
Eduardo Straub <eduardo.straub1@gmail.com>
2019-03-18 21:15:17 -0300

    se agrego el producto

--- yapo/db/schema.rb
+++ yapo/db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_18_231108) do
+ActiveRecord::Schema.define(version: 2019_03_19_001109) do
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "price"
+    t.integer "user_id"
+    t.boolean "sold"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
